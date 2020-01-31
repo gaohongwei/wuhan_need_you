1cf0566ea2b9e7d904d586c61f418b3cdc24fb61
hliuDavis <hliuDavis@gmail.com>
2020-01-30 22:10:55 -0500

    rebuild structure

--- app.py
+++ app.py
@@ -2,9 +2,27 @@
 
 import os
 from flask import *
+from flask_sqlalchemy import SQLAlchemy
 
-
+print(__name__)
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/whu'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+# db.create_all()
+# admin = User(username='admin', email='admin@example.com')
+# guest = User(username='guest', email='guest@example.com')
+# db.session.add(admin)
+# db.session.add(guest)
+# db.session.commit()
 
 
 @app.route('/')
