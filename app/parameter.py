5cbb9c713dfd040b70c0ecbfd1bd61b7ec1ca8b2
hliuDavis <hliuDavis@gmail.com>
2020-01-31 18:52:31 -0500

    dynamic create menu

---
+++ app/parameter.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+# dynamic create menu
+menus = [
+	{
+	    'label': "主页",
+	    'path': '/index'
+  	},
+	{
+	    'label': "捐赠",
+	    'path': '/donate'
+  	},
+	{
+	    'label': "物流",
+	    'path': '/supply'
+  	},
+  	{
+    	'label': "信息公示",
+    	'path': '/info'
+  	}
+]
+
--- app/views.py
+++ app/views.py
@@ -1,13 +1,13 @@
-
 from flask import render_template
 from app import app
+from parameter import *
 
 @app.route('/')
 def index():
-    return render_template('pages/index.html')
+	return render_template('pages/index.html', menus = menus)
 
 @app.route('/<page_name>')
 def menu(page_name):
-	return render_template('pages/' + page_name + '.html')
+	return render_template('pages/' + page_name + '.html', menus = menus)
 
 
