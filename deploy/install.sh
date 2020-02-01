46b222bd0461a294c236ab89d75df6de517e0ded
pkuwwt <wwthunan@gmail.com>
2020-02-02 01:52:22 +0800

    update deploy scripts

--- deploy/install.sh
+++ deploy/install.sh
@@ -6,6 +6,15 @@ mkdir -p $ROOT
 SRC_DIR=`dirname $(readlink -f $0)`/..
 cd $SRC_DIR
 
+# Configure nginx
+apt-get update
+apt install nginx python3
+if ! [ -f /etc/nginx/nginx.conf.bak ]; then
+	cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+fi
+cp deploy/nginx.conf /etc/nginx/
+nginx -s reload
+
 # Install server
 cp -r * $ROOT/
 pip3 install -r requirements.txt
@@ -14,11 +23,5 @@ pip3 install -r requirements.txt
 cp deploy/wuhan_need_you.service /etc/systemd/system/
 systemctl start wuhan_need_you
 systemctl enable wuhan_need_you
+systemctl status wuhan_need_you
 
-# Configure nginx
-apt install nginx-full
-if ! [ -f /etc/nginx/nginx.conf.bak ]; then
-	cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-fi
-cp deploy/nginx.conf /etc/nginx/
-nginx -s reload
