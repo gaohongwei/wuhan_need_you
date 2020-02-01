719312aa401687a0f7468950b2900453aa43d434
edieyoung <370676740@qq.com>
2020-01-31 22:56:15 -0800

    info page

--- app/views.py
+++ app/views.py
@@ -10,4 +10,20 @@ def index():
 def menu(page_name):
 	return render_template('pages/' + page_name + '.html', menus = menus)
 
+@app.route('/info')
+def info():
+	# db = app.config["db"]
+	#info = db.get_info()
+	# a mock info obj list
+	info = [
+		{
+			title: "mock1",
+			desc: "something"
+		},
+		{
+			title: "mock 2",
+			desc: "something"
+		}
+	];
+	return render_template("info.html", info)
 
