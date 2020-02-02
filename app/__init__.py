7c90f4e2893b184f288c53256a98624065b3e494
ironic <yiruowuhen@gmail.com>
2020-02-02 18:10:23 +0800

    Add notices management for admins

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_admin import Admin
+from flask_ckeditor import CKEditor
 from flask_sqlalchemy import SQLAlchemy
+
+from app.model_views.notice import NoticeModelView
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
+admin = Admin(app=app)
+ckeditor = CKEditor(app)
 
 # Load the views
 from app import views
@@ -13,4 +19,4 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 from app.models import User, Notice
 db.create_all()
-
+admin.add_view(NoticeModelView(Notice, db.session))
--- app/views.py
+++ app/views.py
@@ -10,6 +10,12 @@ from app.parameter import *
 def index():
 	return render_template('pages/index.html', menus = menus, pages_info = pages_info)
 
+@app.route('/favicon.ico')
+def favicon():
+
+	# Add favicon
+	return ''
+
 @app.route('/notices')
 def list_notices():
 	total_page =20
@@ -21,4 +27,6 @@ def list_notices():
 @app.route('/<page_name>')
 def menu(page_name):
 	return render_template('pages/' + page_name + '.html', menus = menus, pages_info = pages_info)
+
+
 
