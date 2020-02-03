f54c2c9e7dd56dde53366d9c8a5a7ad5e1991259
hliuDavis <hliuDavis@gmail.com>
2020-02-02 19:10:26 -0500

    add 4 pages

--- app/parameter.py
+++ app/parameter.py
@@ -35,7 +35,7 @@ menus = [
 # information about index pages
 index_info = {
 
-    'home_images': ["image/alumni_activities/2018_bbq.jpg", "image/alumni_activities/2017_president.jpg", "image/wuhan_situation/h1.jpg", "image/wuhan_situation/h6.jpg"],
+    'home_images': ["image/alumni_activities/2018_bbq.jpg", "image/alumni_activities/2017_president.jpg", "image/alumni_action/h1.jpg", "image/alumni_action/h6.jpg"],
 
     'alumni_association': {
       'title': "武汉大学北加州校友会",
@@ -74,83 +74,83 @@ alumni_activities_info = {
   'activites_info': [
     {
       'title': "2019年10月 校友会年度BBQ (拔河掠影)",
-      'image': ["image/alumni_activities/2019_life_science_1.jpg", "image/alumni_activities/2019_life_science_2.jpg"]
+      'image': ["image/alumni_activities/2019_bbq_1.jpg", "image/alumni_activities/2019_bbq_2.jpg"]
     }, 
     {
       'title': "2019年10月 校友会协办3551国际创业大赛(硅谷赛区)",
-      'image': "image/alumni_activities/2019_3551.jpg"
+      'image': ["image/alumni_activities/2019_3551.jpg"]
     }, 
     {
       'title': "2019年8月校友会对接生命科学院暑期访学团",
-      'image': ""
+      'image': ["image/alumni_activities/2019_life_science_1.jpg", "image/alumni_activities/2019_life_science_2.jpg"]
     }, 
     {
       'title': "2018年12 武大校友同武大院士教授欢聚",
-      'image': ""
+      'image': ["image/alumni_activities/2018_prof.jpg"]
     }, 
     {
       'title': "2018年10月 校友会年度BBQ合影",
-      'image': ""
+      'image': ["image/alumni_activities/2018_bbq.jpg"]
     }, 
     {
       'title': "2018年10月校友会协办3551国际创业大赛(硅谷赛区)",
-      'image': ""
+      'image': ["image/alumni_activities/2018_3551.jpg"]
     }, 
     {
       'title': "2018年8月校友会对接生命科学院暑期访学团",
-      'image': ""
+      'image': ["image/alumni_activities/2018_life_science.jpg"]
     }, 
     {
       'title': "2018年8月校友会代表队参与第16届北加州华人文化体育协会运动大会",
-      'image': ""
+      'image': ["image/alumni_activities/2018_sport_meet_1.jpg", "image/alumni_activities/2018_sport_meet_2.jpg"]
     }, 
     {
       'title': "2018年4月 武大校董喻鹏和新一届校友会成员一起",
-      'image': ""
+      'image': ["image/alumni_activities/2018_university_council.jpg"]
     }, 
     {
       'title': "2017年 窦贤康校长与校友会在斯坦福组织武大海外恳谈会",
-      'image': ""
+      'image': ["image/alumni_activities/2017_president.jpg"]
     }, 
     {
       'title': "2017年武大校友会全体人员",
-      'image': ""
+      'image': ["image/alumni_activities/2017_alumni.jpg"]
     }, 
     {
       'title': "2017年校友会年度BBD(联合兄弟校友会和湖北同乡会)",
-      'image': ""
+      'image': ["image/alumni_activities/2017_bbq.jpg"]
     }, 
     {
       'title': "2015年11月 武汉大学副校长黄泰岩 在湾区招才引智",
-      'image': ""
+      'image': ["image/alumni_activities/2015_vice_president.jpg"]
     }, 
     {
       'title': "2010年 武大校长刘经南到湾区看望武大校友",
-      'image': ""
+      'image': ["image/alumni_activities/2010_president.jpg"]
     }, 
     {
       'title': "2007年12月 黄彰任会长（前排居中），基金会成员与彭旧金山总领事合影",
-      'image': ""
+      'image': ["image/alumni_activities/2017_admin.jpg"]
     }, 
     {
       'title': "2007年时任武汉市委书记（现任工信部部长）苗圩-左三与基金会部分成员（欧阳明-右二，王高峰-右一）合影",
-      'image': ""
+      'image': ["image/alumni_activities/2007_wuhan_maire.jpg"]
     }, 
     {
       'title': "2007年 刘经南 校长访问基金会留影",
-      'image': ""
+      'image': ["image/alumni_activities/2007_president.jpg"]
     }, 
     {
       'title': "2006年 校友会校友年度BBQ",
-      'image': ""
+      'image': ["image/alumni_activities/2006_bbq.jpg"]
     }, 
     {
       'title': "2005年宁挺，欧阳明，王高峰等代表基金会参与华人运动会",
-      'image': ""
+      'image': ["image/alumni_activities/2005_sport_meet.jpg"]
     }, 
     {
       'title': "1998年校友会成立大会合影留念",
-      'image': ""
+      'image': ["image/alumni_activities/1998_setup.jpg"]
     }
 
   ]
@@ -159,12 +159,44 @@ alumni_activities_info = {
 
 # information about wuhan situation pages
 wuhan_situation_info = {
-  
+  'introduction': "武汉市，一个人口1100万的城市，中国中部最大的都市。 2019年12月，武汉地区发现了一种新的冠状病毒。为了限制病毒传播，整座城市正处于封锁状态。到目前为止，该病毒已影响数千名武汉居民，并造成数百人死亡。 \
+                  武汉处于紧急状态。武汉需要您的帮助。\
+                  我们正在努力支持武汉的医疗物资，并提供对该疾病的报道和认识。",
+  'china_update': 'image/wuhan_situation/wuhan_update_203.jpg',
+  'source': {
+    'title': "来源:",
+    'content': "新浪",
+    'link': "https://news.sina.cn/zt_d/yiqing0121?vt=4&pos=undefined"
+  }
 }
 
 # information about alumni action pages
 alumni_action_info = {
-
+  'actions': [
+      {
+        'title': "21小时接力护送！2.5吨医用物资从旧金山直接运抵武汉协和医院",
+        'image_info': {
+           'link': "image/alumni_action/h1.jpg",
+           'title': "协和医院和武大中南医院接受捐赠物资"
+          },
+        'content': "人民网旧金山1月31日电（记者邓圩）21小时接力护送！北京时间1月31日下午，由WuhanUnited(武大和华科北加州校友会）联合国际直接援助组织(Direct Relief)捐赠的2.75万双医用手套，4000件隔离服，20万个外科手术口罩，重达2.5吨货物从旧金山直接运抵武汉协和医院。\
+                    “武汉加油” 北加州在行动 \
+                    武汉加油行动（Wuhan United）是一支心系湖北的在美华人组成的志愿者团队。行动的发起者是旧金山湾区的武汉大学北加州校友会和华科北加州校友会，包括硅谷高科技公司工程师、大学教授及医疗工作者等多个领域的专业人士。对这些曾就学于武汉的莘莘学子，武汉对于每一个成员来说都有如家一般的存在。\
+                    区别于购买市面上散货的方式，武汉加油行动团队成立之初，一直在寻求与非政府组织合作的机会。他们与Direct Relief（国际直接援助组织）与达成战略合作，搭建了一座跨国桥梁，将非政府组织所拥有的医疗捐赠物资直接送达武汉抗击疫情一线。\
+                    货车开往机场路上还在完成通关手续\
+                    21小时完成不可能的任务，背后是与时间赛跑的故事。\
+                    美西时间与北京时间有16小时时差。从志愿者团队与Direct Relief最终达成合作，到物流飞机起飞，仅仅剩下一个周日和半个工作日。\
+                    旧金山当地时间1月27日下午3点，救助武汉协和医院的物资离开仓库，前往机场，这个时间在中国还是半夜。团队需要完成捐赠方、承运方、接收方、海关清关以及报关公司5个单位所有的文书材料，无疑是与时间赛跑。\
+                    捐赠物资的海关手续，在货车开往机场和飞机飞越太平洋的途中，在中美两地紧锣密鼓地进行着。\
+                    美西时间1月28日，北京时间1月29日凌晨，运送物资的飞机抵达广州,完成清关手续抵达中国邮政广州转运中心，由武汉大学广州校友会全程护送，当晚10点中国邮政EMS货车启程武汉。",
+      
+        'source': {
+            'title': "来源: ",
+            'content': "人民网-国际频道",
+            'link': "http://world.people.com.cn/n1/2020/0201/c1002-31566128.html",
+        }
+      }
+  ]
 }
 
 # information about donate pages
@@ -172,7 +204,7 @@ donate_info = {
   
 }
 
-# information about contact us pages
+# information about contact21小时完成不可能的任务，背后是与时间赛跑的故事。
 contact_us_info = {
   
 }
