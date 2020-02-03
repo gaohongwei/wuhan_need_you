b65a93a1bbb9029d69c12c832ad920033eaad42f
ironic <yiruowuhen@gmail.com>
2020-02-03 21:41:33 +0800

    Add list notices and notice detail page

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# Enable Flask's debugging features. Should be False in production
+DEBUG = True
+
+# SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/whu'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///./test2.db'
