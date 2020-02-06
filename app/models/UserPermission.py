2d7232e06a1e1eabfcf057061fc1b9b444a8bbc9
pkuwwt <wwthunan@gmail.com>
2020-02-06 13:18:52 +0800

    update deploy scripts; update permission

--- app/models/UserPermission.py
+++ app/models/UserPermission.py
@@ -2,6 +2,9 @@
 
 from flask import request, current_app
 
+def get_current_route():
+    return request.script_root + request.path
+
 class UserPermission:
     def __init__(self):
         pass
@@ -19,10 +22,7 @@ class RoutePermission(UserPermission):
         return allow_role_name != None and allow_role_name(self.role_name) and self.match_route(self.route)
     @classmethod
     def match_route(cls, route):
-        return route == RoutePermission.get_current_route()
-    @classmethod
-    def get_current_route(cls):
-        return request.script_root + request.path
+        return route == get_current_route()
 
 class ModelViewPermission(UserPermission):
     def __init__(self, modelViewClass, role_name):
@@ -34,22 +34,23 @@ class ModelViewPermission(UserPermission):
         allow_role_name = getattr(user, 'allow_role_name', None)
         return allow_role_name != None and user.allow_role_name(self.role_name)
 
-route_permissions = []
+route_permissions = {}
 model_view_permissions = {}
 
 def register_route_permission(route, role_name):
-    route_permissions.append(RoutePermission(route, role_name))
+    route_permissions[route] = RoutePermission(route, role_name)
 
 def register_model_view_permission(modelViewClass, role_name):
     model_view_permissions[modelViewClass] = ModelViewPermission(modelViewClass, role_name)
 
 def check_route_permission(user):
-    for permission in route_permissions:
-        if permission.allow(user):
-            current_app.logger.info('permitted')
-            return True
-    current_app.logger.info('not permitted')
-    return False
+    if user == None:
+        return False
+    permission = route_permissions.get(get_current_route())
+    if permission == None:
+        current_app.logger.info('not permitted')
+        return False
+    return permission.allow(user)
 
 def check_model_view_permission(modelView, user):
     if modelView == None or user == None:
